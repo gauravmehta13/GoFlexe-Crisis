5e447ab435574e4ffa750179d7276a294d944fe7
Gaurav <270mehta@gmail.com>
2021-05-17 16:26:01 +0530

    added filters in twitter page

--- lib/Screens/Twitter Search.dart
+++ lib/Screens/Twitter Search.dart
@@ -11,6 +11,18 @@ class TwitterScreen extends StatefulWidget {
 class _TwitterScreenState extends State<TwitterScreen> {
   ScrollController _scrollController = ScrollController();
   String city = "";
+  bool beds = true;
+  bool ventilator = true;
+  bool icu = true;
+  bool oxygen = true;
+  bool tests = false;
+  bool fabiflu = false;
+  bool remdesivir = false;
+  bool favipiravir = false;
+  bool tocilizumab = false;
+  bool plasma = false;
+  bool food = false;
+  bool ambulance = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +37,7 @@ class _TwitterScreenState extends State<TwitterScreen> {
           child: Link(
               target: LinkTarget.blank,
               uri: Uri.parse(
-                  "https://twitter.com/search?q=verified+$city+(bed+OR+beds+OR+icu+OR+oxygen+OR+ventilator+OR+ventilators+OR+fabiflu)+-%22not+verified%22+-%22unverified%22+-%22needed%22+-%22required%22&f=live"),
+                  "https://twitter.com/search?q=verified+$city+(${beds == true ? "bed+OR+beds+OR" : ""}+${icu == true ? "icu+OR" : ""}+${oxygen == true ? "oxygen+OR" : ""}+${ventilator == true ? "ventilator+OR+ventilators+OR" : ""}+${tests == true ? "test+OR+tests+OR+testing+OR" : ""}+${fabiflu == true ? "fabiflu+OR" : ""}+${favipiravir == true ? "favipiravir+OR" : ""}+${remdesivir == true ? "remdesivir+OR" : ""}+${tocilizumab == true ? "tocilizumab+OR" : ""}+${plasma == true ? "plasma+OR" : ""}+${food == true ? "food+OR+tiffin+OR" : ""}+${ambulance == true ? "ambulance" : ""})+%20-not%20verified%20-unverified%20-needed%20-need%20-needs%20-required%20-require%20-requires%20-requirement%20-requirements&src=typeahead_click&f=live"),
               builder: (context, followLink) => ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFf9a825), // background
@@ -43,7 +55,7 @@ class _TwitterScreenState extends State<TwitterScreen> {
         controller: _scrollController,
         child: Container(
           height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.start,
@@ -65,44 +77,307 @@ class _TwitterScreenState extends State<TwitterScreen> {
               ),
               box30,
               box30,
-              Autocomplete<String>(
-                displayStringForOption: (option) => option,
-                fieldViewBuilder: (context, textEditingController, focusNode,
-                        onFieldSubmitted) =>
-                    TextField(
-                  controller: textEditingController,
-                  focusNode: focusNode,
-                  scrollPadding: const EdgeInsets.only(bottom: 150.0),
-                  // onEditingComplete: onFieldSubmitted,
-                  decoration: const InputDecoration(
-                      isDense: true,
-                      border: OutlineInputBorder(),
-                      hintText: "Search City"),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Autocomplete<String>(
+                  displayStringForOption: (option) => option,
+                  fieldViewBuilder: (context, textEditingController, focusNode,
+                          onFieldSubmitted) =>
+                      TextField(
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    scrollPadding: const EdgeInsets.only(bottom: 150.0),
+                    // onEditingComplete: onFieldSubmitted,
+                    decoration: const InputDecoration(
+                        isDense: true,
+                        border: OutlineInputBorder(),
+                        hintText: "Search City"),
+                  ),
+                  optionsBuilder: (textEditingValue) {
+                    if (textEditingValue.text == '') {
+                      return cities;
+                    }
+                    return cities.where((String option) {
+                      return option
+                          .toLowerCase()
+                          .contains(textEditingValue.text.toLowerCase());
+                    });
+                  },
+                  onSelected: (String selection) {
+                    final FocusScopeNode currentScope = FocusScope.of(context);
+                    if (!currentScope.hasPrimaryFocus &&
+                        currentScope.hasFocus) {
+                      FocusManager.instance.primaryFocus.unfocus();
+                    }
+                    print(selection);
+                    setState(() {
+                      city = selection;
+                    });
+                    _scrollController.animateTo(
+                        _scrollController.position.minScrollExtent,
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.ease);
+                  },
                 ),
-                optionsBuilder: (textEditingValue) {
-                  if (textEditingValue.text == '') {
-                    return cities;
-                  }
-                  return cities.where((String option) {
-                    return option
-                        .toLowerCase()
-                        .contains(textEditingValue.text.toLowerCase());
-                  });
-                },
-                onSelected: (String selection) {
-                  final FocusScopeNode currentScope = FocusScope.of(context);
-                  if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
-                    FocusManager.instance.primaryFocus.unfocus();
-                  }
-                  print(selection);
-                  setState(() {
-                    city = selection;
-                  });
-                  _scrollController.animateTo(
-                      _scrollController.position.minScrollExtent,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease);
-                },
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / 3 - 10,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: beds,
+                                onChanged: (value) {
+                                  setState(() {
+                                    beds = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Beds",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: ventilator,
+                                onChanged: (value) {
+                                  setState(() {
+                                    ventilator = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Ventilator",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: fabiflu,
+                                onChanged: (value) {
+                                  setState(() {
+                                    fabiflu = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Fabiflu",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: remdesivir,
+                                onChanged: (value) {
+                                  setState(() {
+                                    remdesivir = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Remdesivir",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 3 - 10,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: icu,
+                                onChanged: (value) {
+                                  setState(() {
+                                    icu = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "ICU",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: plasma,
+                                onChanged: (value) {
+                                  setState(() {
+                                    plasma = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Plasma",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: favipiravir,
+                                onChanged: (value) {
+                                  setState(() {
+                                    favipiravir = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Favipiravir",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: food,
+                                onChanged: (value) {
+                                  setState(() {
+                                    food = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Food",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 3 - 10,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: oxygen,
+                                onChanged: (value) {
+                                  setState(() {
+                                    oxygen = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Oxygen",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: ambulance,
+                                onChanged: (value) {
+                                  setState(() {
+                                    ambulance = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Ambulance",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: tocilizumab,
+                                onChanged: (value) {
+                                  setState(() {
+                                    tocilizumab = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Tocilizumab",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                value: tests,
+                                onChanged: (value) {
+                                  setState(() {
+                                    tests = value;
+                                  });
+                                },
+                              ),
+                              Text(
+                                "Tests",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w600),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
